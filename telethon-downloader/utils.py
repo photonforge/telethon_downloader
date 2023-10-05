43d6cb5835ab7e6d6498202113132891ab106f58
photonforge <50198919+photonforge@users.noreply.github.com>
2023-10-05 20:44:48 +0530

    Update utils.py
    MOV files to mp4 directory

--- telethon-downloader/utils.py
+++ telethon-downloader/utils.py
@@ -137,6 +137,7 @@ def config_file():
 		config['DEFAULT_PATH']['flac'] = '/download/mp3'
 		config['DEFAULT_PATH']['jpg'] = '/download/jpg'
 		config['DEFAULT_PATH']['mp4'] = '/download/mp4'
+		config['DEFAULT_PATH']['mov'] = '/download/mp4'
 
 		config['REGEX_PATH'] = {}
 		config['REGEX_PATH']['/example/i'] = '/download/example'
